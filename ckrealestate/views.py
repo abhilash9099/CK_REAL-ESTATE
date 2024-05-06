627d7a1a38a1f393a2f52ff20e7b682f388ee68b
nsavaram08 <143136483+nsavaram08@users.noreply.github.com>
2024-05-06 17:28:28 -0500

    adding final changes

--- ckrealestate/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
-
-@login_required
-def LogoutView(request):
-    logout(request)
-    return render(request, 'registration/logged_out.html')
---
+++ property/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Neibhorhood, PropertyType
+
+# Register your models here.
+admin.site.register(Neibhorhood)
+admin.site.register(PropertyType)
