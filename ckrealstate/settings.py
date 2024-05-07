905002c87346473968be5063b337883d5e80a4b8
nsavaram08 <143136483+nsavaram08@users.noreply.github.com>
2024-05-07 18:02:37 -0500

    Static change

--- ckrealstate/settings.py
+++ ckrealstate/settings.py
@@ -122,11 +122,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-  
-]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -148,5 +143,6 @@ LOOUT_REDIRECT_URL = '/accounts/login'
 
 import os
 MEDIA_URL = 'media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
